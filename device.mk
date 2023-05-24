442981feb0bb0a47e8757add210d2d1468842a09
Raunak478 <122750258+Raunak478@users.noreply.github.com>
2023-05-24 18:32:12 +0530

    Added Pixel 2016 exclusive xml

--- device.mk
+++ device.mk
@@ -166,6 +166,11 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-service \
     android.hardware.gatekeeper@1.0.vendor
 
+# Google Photos
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/pixel_2016_exclusive.xml
+
+
 # GPS / Location
 include $(LOCAL_PATH)/gps/gps_vendor_product.mk
 
